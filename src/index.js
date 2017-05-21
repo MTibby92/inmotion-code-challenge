72bc234485d3293406a91c6954d7b0c5b6de015b
Matt Tibby <MTibby92@gmail.com>
2017-05-21 00:00:28 -0500

    refactored code to have search component present in the router; allowed the search button to redirect to the /search link and display the results there; search page uses the MoveList component to render now, with props preventing certain features like deletion and adding new movies from being rendered

--- src/index.js
+++ src/index.js
@@ -12,41 +12,59 @@ import {
 	this section will always be present, allowing the user to search
 	for a movie at any point
 */
-const SearchContainer = (props) => {
-	return (
-		<div className="jumbotron">
-			<h1>Search your movie collection</h1>
+class SearchContainer extends React.Component {
+	constructor(props) {
+		super(props)
+	}
 
-			<form className="form-inline" onSubmit={props.onSubmit}>
-				<label className="sr-only" htmlFor="movieSearchBar">Movie Search Bar</label>
-				<div className="input-group" >
-					<div className="input-group-addon">
-						<i className="fa fa-search" aria-hidden="true"></i>
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.context.router.history.push('/search')
+		this.props.onSubmit()
+	}
+
+	render() {
+		return (
+			<div className="jumbotron">
+				<h1>Search your movie collection</h1>
+
+				<form className="form-inline" onSubmit={this.handleSubmit}>
+					<label className="sr-only" htmlFor="movieSearchBar">Movie Search Bar</label>
+					<div className="input-group" >
+						<div className="input-group-addon">
+							<i className="fa fa-search" aria-hidden="true"></i>
+						</div>
+						<input type="text"
+							className="form-control mr-sm-2"
+							id="movieSearchBar"
+							placeholder="Search..."
+							value={this.props.inputValue}
+						 	onChange={this.props.onInputChange} />
 					</div>
-					<input type="text"
-						className="form-control mr-sm-2"
-						id="movieSearchBar"
-						placeholder="Search..."
-						value={props.inputValue}
-					 	onChange={props.onInputChange} />
-				</div>
 
-				<label className="mr-sm-2" htmlFor="movieSearchDropdown">By:</label>
-				<select className="custom-select mb-2 mr-sm-2 mb-sm-0"
-					id="movieSearchDropdown"
-					defaultValue={props.selectValue}
-					onChange={props.onSelectChange} >
-					<option value="title">Title</option>
-					<option value="genre">Genre</option>
-					<option value="year">Year</option>
-				</select>
+					<label className="mr-sm-2" htmlFor="movieSearchDropdown">By:</label>
+					<select className="custom-select mb-2 mr-sm-2 mb-sm-0"
+						id="movieSearchDropdown"
+						defaultValue={this.props.selectValue}
+						onChange={this.props.onSelectChange} >
+						<option value="title">Title</option>
+						<option value="genre">Genre</option>
+						<option value="year">Year</option>
+					</select>
 
-				<button type="submit" className="btn btn-primary" >
-					Search
-				</button>
-			</form>
-		</div>
-	)
+					<button type="submit" className="btn btn-primary" >
+						{/* <Link to="/search" style={{color: 'white', textDecoration: 'none'}} >Search</Link> */}
+						Search
+					</button>
+				</form>
+			</div>
+		)
+	}
+}
+SearchContainer.contextTypes = {
+	router: React.PropTypes.shape({
+	    history: React.PropTypes.object.isRequired,
+	  })
 }
 
 /*
@@ -102,9 +120,84 @@ class ResultsRouter extends React.Component {
 				year: '',
 				actors: '',
 				rating: 'PG'
-			}
+			},
+			searchData: {
+				searchQuery: '',
+				searchType: 'title'
+			},
+			searchResults: []
 		}
 	}
+
+	handleSearchInputChange = (e) => {
+		console.log(e.target.value)
+		this.setState({
+			searchData: {
+				...this.state.searchData,
+				searchQuery: e.target.value
+			}
+		})
+	}
+
+	handleSearchSelectChange = (e) => {
+		console.log(e.target.value)
+		this.setState({
+			searchData: {
+				...this.state.searchData,
+				searchType: e.target.value
+			}
+		})
+	}
+
+	handleSearchSubmit = (e) => {
+		// this is where either the route redirect will happen using withRouter
+		// and history.push() or a state change to display the results
+		let tempSearchResults = []
+		let movies = this.state.movies
+		let query = this.state.searchData
+		switch(query.searchType) {
+			case 'title':
+				console.log('Made it to title switch')
+				movies.map((movie) => {
+					if(movie.title == query.searchQuery) {
+						tempSearchResults.push(movie)
+					}
+				})
+
+				console.log(tempSearchResults)
+				break
+			case 'genre':
+				console.log('Made it to genre switch')
+				movies.map((movie) => {
+					if(movie.genre == query.searchQuery) {
+						tempSearchResults.push(movie)
+					}
+				})
+				console.log(tempSearchResults)
+				break
+			case 'year':
+				console.log('Made it to year switch')
+				movies.map((movie) => {
+					if(movie.year == query.searchQuery) {
+						tempSearchResults.push(movie)
+					}
+				})
+				console.log(tempSearchResults)
+				break
+			default:
+				console.log('default')
+				return
+		}
+		this.setState({
+			searchResults: tempSearchResults,
+			searchData: {
+				searchQuery: '',
+				searchType: 'title'
+			}
+		})
+	}
+
+	// =================Search Data above, Add data below==============
 
 	handleAddMovieChange = (e) => {
 		switch (e.target.id) {
@@ -182,17 +275,35 @@ class ResultsRouter extends React.Component {
 	render() {
 		return (
 	   		 <Router>
-	   			 <Switch>
-					 <Route exact path="/" component={ShowListButton} />
-	   				 <Route path="/search" render={() => <SearchResults movies={this.state.movies} />} />
-	   				 <Route path="/list" render={() =>
-						 <MovieList movies={this.state.movies}
-							 onChange={this.handleAddMovieChange}
-							 onSubmit={this.handleAddMovieSubmit}
-							 onDelete={this.handleMovieDelete}
-							 onEdit={this.handleMovieEdit}
-							 value={this.state.newMovie} />} />
-	   			 </Switch>
+				 <div className="container">
+					 <SearchContainer
+	 					onInputChange={this.handleSearchInputChange}
+	 					inputValue={this.state.searchData.searchQuery}
+	 					selectValue={this.state.searchData.searchType}
+	 				 	onSelectChange={this.handleSearchSelectChange}
+	 					onSubmit={this.handleSearchSubmit} />
+					<Switch>
+	   					 <Route exact path="/" component={ShowListButton} />
+	   	   				 <Route path="/search" render={() =>
+							 <MovieList movies={this.state.searchResults}
+								 onChange={this.handleAddMovieChange}
+								 onSubmit={this.handleAddMovieSubmit}
+								 onDelete={this.handleMovieDelete}
+								 onEdit={this.handleMovieEdit}
+								 pageTitle="Search Results"
+								 componentType="Search Results"
+								 value={this.state.newMovie} />} />
+	   	   				 <Route path="/list" render={() =>
+	   						 <MovieList movies={this.state.movies}
+	   							 onChange={this.handleAddMovieChange}
+	   							 onSubmit={this.handleAddMovieSubmit}
+	   							 onDelete={this.handleMovieDelete}
+	   							 onEdit={this.handleMovieEdit}
+								 pageTitle="My Movie List"
+								 componentType="Movie List"
+	   							 value={this.state.newMovie} />} />
+	   	   			 </Switch>
+				 </div>
 	   		 </Router>
    	 	)
 	}
@@ -203,23 +314,12 @@ const ShowListButton = (props) => {
 	return (
 		<div className="row">
 			<div className="col-sm-2 offset-sm-5">
-				<button className="btn">
-					<Link to="/list">View List of Movies</Link>
+				<button className="btn btn-primary">
+					<Link to="/list" style={{color: 'white', textDecoration: 'none'}}>View List of Movies</Link>
 				</button>
 			</div>
 		</div>
 
-	)
-}
-
-
-const SearchResults = (props) => {
-	return (
-		<div className="row">
-			<div className="col-sm-8">
-				<h3>Search Results go here</h3>
-			</div>
-		</div>
 	)
 }
 
@@ -231,75 +331,100 @@ const SearchResults = (props) => {
 const MovieList = (props) => {
 	return (
 		<div className="row">
+			{props.pageTitle == 'Search Results' ? (
+				<div className="col-sm-2 offset-sm-5">
+					<button className="btn btn-primary mb-3">
+						<Link to="/list" style={{color: 'white', textDecoration: 'none'}} >Go to Full List</Link>
+					</button>
+				</div>
+			) : (
+				null
+			)}
+
 			<div className="col-sm-6 offset-sm-3">
-				<h2 className="text-center" style={{marginBottom: '1rem'}} >My Movie List</h2>
+				{/* Page Title is dependent on whether it was searched for or the whole list */}
+				<h2 className="text-center" style={{marginBottom: '1rem'}} >{props.pageTitle}</h2>
+
 				<div className="" >
 					{props.movies.map((movie, i) =>
-						<MovieCard key={i} index={i} data={movie} onDelete={props.onDelete} onEdit={props.onEdit} onCardSubmit={props.onCardSubmit} />
+						<MovieCard key={i}
+							index={i}
+							data={movie}
+							componentType={props.componentType}
+							onDelete={props.onDelete}
+							onEdit={props.onEdit}
+							onCardSubmit={props.onCardSubmit} />
 					)}
 				</div>
 
-				<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#addNewMovieModal">
-					Add New Movie
-				</button>
+				{/* the add new movie button is only available when the /list route is hit, not the
+					search route */}
+				{props.pageTitle == 'My Movie List' ? (
+					<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#addNewMovieModal">
+						Add New Movie
+					</button>
+				) : (
+					null
+				)}
 
 
-				<div className="modal fade" id="addNewMovieModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div className="modal-dialog" role="document">
-						<div className="modal-content">
-							<div className="modal-header">
-				        		<h5 className="modal-title" id="exampleModalLabel">Add New Movie</h5>
-				        		<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-				          			<span aria-hidden="true">&times;</span>
-				        		</button>
-				      		</div>
-							<form onSubmit={props.onSubmit} >
-				      			<div className="modal-body">
-          							<div className="form-group">
-							            <label htmlFor="movie-title" className="form-control-label">Movie Title:</label>
-							            <input required type="text" className="form-control" id="movie-title" onChange={props.onChange} value={props.value.title} />
-							        </div>
-									<div className="form-group">
-							            <label htmlFor="genre" className="form-control-label">Genre:</label>
-							            <input required type="text" className="form-control" id="genre" onChange={props.onChange} value={props.value.genre} />
-							        </div>
-									<div className="form-group">
-							            <label htmlFor="year" className="form-control-label">Year Released:</label>
-							            <input required type="number" className="form-control" id="year" onChange={props.onChange} value={props.value.year} />
-							        </div>
-									<div className="form-group">
-							            <label htmlFor="actors" className="form-control-label">Leading Actors:</label>
-							            <input required type="text" className="form-control" id="actors" onChange={props.onChange} value={props.value.actors} />
-							        </div>
-									<div className="form-group">
-							            <label htmlFor="rating" className="form-control-label">Movie Rating:</label>
-										<select className="form-control custom-select"
-											id="rating"
-
-											required
-											onChange={props.onChange}
-											value={props.value.rating}>
-											<option value="G">G</option>
-											<option value="PG">PG</option>
-											<option value="PG-13">PG-13</option>
-											<option value="R">R</option>
-										</select>
-							        </div>
+				{/* modal only needs to be rendered on the full list route, not the search route */}
+				{props.pageTitle == 'My Movie List' ? (
+					<div className="modal fade" id="addNewMovieModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div className="modal-dialog" role="document">
+							<div className="modal-content">
+								<div className="modal-header">
+					        		<h5 className="modal-title" id="exampleModalLabel">Add New Movie</h5>
+					        		<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+					          			<span aria-hidden="true">&times;</span>
+					        		</button>
 					      		</div>
-					      		<div className="modal-footer">
-					        		<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-					        		<button type="submit" className="btn btn-primary" data-toggle="modal"
-										data-target="#addNewMovieModal">Add Movie</button>
-					      		</div>
-							</form>
-				    	</div>
-				  	</div>
-				</div>
+								<form onSubmit={props.onSubmit} >
+					      			<div className="modal-body">
+	          							<div className="form-group">
+								            <label htmlFor="movie-title" className="form-control-label">Movie Title:</label>
+								            <input required type="text" className="form-control" id="movie-title" onChange={props.onChange} value={props.value.title} />
+								        </div>
+										<div className="form-group">
+								            <label htmlFor="genre" className="form-control-label">Genre:</label>
+								            <input required type="text" className="form-control" id="genre" onChange={props.onChange} value={props.value.genre} />
+								        </div>
+										<div className="form-group">
+								            <label htmlFor="year" className="form-control-label">Year Released:</label>
+								            <input required type="number" className="form-control" id="year" onChange={props.onChange} value={props.value.year} />
+								        </div>
+										<div className="form-group">
+								            <label htmlFor="actors" className="form-control-label">Leading Actors:</label>
+								            <input required type="text" className="form-control" id="actors" onChange={props.onChange} value={props.value.actors} />
+								        </div>
+										<div className="form-group">
+								            <label htmlFor="rating" className="form-control-label">Movie Rating:</label>
+											<select className="form-control custom-select"
+												id="rating"
 
+												required
+												onChange={props.onChange}
+												value={props.value.rating}>
+												<option value="G">G</option>
+												<option value="PG">PG</option>
+												<option value="PG-13">PG-13</option>
+												<option value="R">R</option>
+											</select>
+								        </div>
+						      		</div>
+						      		<div className="modal-footer">
+						        		<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+						        		<button type="submit" className="btn btn-primary" data-toggle="modal"
+											data-target="#addNewMovieModal">Add Movie</button>
+						      		</div>
+								</form>
+					    	</div>
+					  	</div>
+					</div>
+				) : (
+					null
+				)}
 			</div>
-
-
-
 		</div>
 	)
 }
@@ -320,7 +445,13 @@ const MovieCard = (props) => {
 				<p className="card-text" >Lead Actors: {props.data.actors}</p>
 				<p className="card-text" >Rating: {props.data.rating}</p>
 				{/* <button className="btn btn-primary" data-index={props.index} onClick={props.onEdit} >Edit</button> */}
-				<button className="btn btn-danger ml-2" data-index={props.index} onClick={props.onDelete} >Delete</button>
+
+				{/* only render delete button if it's part of the full movie list */}
+				{props.componentType == 'Movie List' ? (
+					<button className="btn btn-danger ml-2" data-index={props.index} onClick={props.onDelete} >Delete</button>
+				) : (
+					null
+				)}
 			</div>
 		</div>
 	)
@@ -331,52 +462,10 @@ const MovieCard = (props) => {
 	this component will host all the other components and the highest level
 	state necessary
 */
-class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			searchQuery: '',
-			searchType: 'title'
-		}
-	}
-
-	handleSearchInputChange = (e) => {
-		console.log(e.target.value)
-		this.setState({
-			searchQuery: e.target.value
-		})
-	}
-
-	handleSearchSelectChange = (e) => {
-		console.log(e.target.value)
-		this.setState({
-			searchType: e.target.value
-		})
-	}
-
-	handleSearchSubmit = (e) => {
-		e.preventDefault()
-		// this is where either the route redirect will happen using withRouter
-		// and history.push() or a state change to display the results
-		this.setState({
-			searchQuery: '',
-			searchType: 'title'
-		})
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<SearchContainer
-					onInputChange={this.handleSearchInputChange}
-					inputValue={this.state.searchQuery}
-					selectValue={this.state.searchType}
-				 	onSelectChange={this.handleSearchSelectChange}
-					onSubmit={this.handleSearchSubmit} />
-				<ResultsRouter />
-			</div>
-		)
-	}
+const App = (props) => {
+	return (
+		<ResultsRouter />
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
